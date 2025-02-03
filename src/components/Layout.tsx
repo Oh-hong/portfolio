e688cc181ed290699761c8d8ace218e0bf1e919c
Oh_hong <jungi9933@naver.com>
2025-02-03 15:05:42 +0900

    speedInsights 오류 해결?

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
