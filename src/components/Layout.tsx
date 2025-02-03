c45858d1339b1ca24e501a6b6b931c35edf58756
Oh_hong <jungi9933@naver.com>
2025-02-03 14:31:16 +0900

    speedInsights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-w-4xl mx-auto p-8 flex flex-col gap-28 md:gap-32 md:my-20 my-4 mb-20">
       {children}
+      <SpeedInsights />
     </div>
   );
 };
